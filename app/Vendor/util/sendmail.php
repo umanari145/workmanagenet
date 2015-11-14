7499026fc81b4aa2fab07fe8761de1b36b0fa37a
Norio <umanari145@gmail.com>
2015-11-14 15:00:58 +0900

    メールテスト修正

--- app/Vendor/util/sendmail.php
+++ app/Vendor/util/sendmail.php
@@ -5,7 +5,7 @@ class Sendmail{
 	public function sendGridMail(){
 		$sendgrid = new SendGrid( EMAIL_USER_NAME, EMAIL_PASSWORD );
 		$email = new SendGrid\Email();
-		$email->addTo('matsumoto@donow.jp')->
+		$email->addTo('umanari145@gmail.com')->
 		setFrom('matsumoto@donow.jp')->
 		setSubject('件名')->
 		setText('こんにちは！');
