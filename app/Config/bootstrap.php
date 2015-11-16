1dd1d368f8dc7e088ab1cbaea50118c56d9e9748
Matsumoto Norio <matsumoto@donow.jp>
2015-11-16 17:08:46 +0900

    メール送信先を山本さんに戻す

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -6,7 +6,7 @@ include __DIR__ .'/bootstrap/environments.php';
 
 
 define("COMMENT_LENGTH", 20);
-define("ADMIN_EMAIL_ADDRESS","matsumoto@donow.jp");
+define("ADMIN_EMAIL_ADDRESS","yamamoto@donow.jp");
 
 Configure::write('Dispatcher.filters', array(
     'AssetDispatcher',
